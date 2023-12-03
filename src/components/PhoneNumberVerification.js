bc4b0615916ceded397d10b0ba0b3032bd7b87b7
lsaffie <luis@saffie.ca>
2023-12-02 21:54:27 -0500

    change path for import

--- src/components/PhoneNumberVerification.js
+++ src/components/PhoneNumberVerification.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import appConfig from './config';
+import appConfig from '../config';
 
 // Helper Functions
 const formatToE164 = (phone) => {
