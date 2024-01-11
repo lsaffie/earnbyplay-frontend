00bd240cda300dfdc97a806e47cac94010880133
lsaffie <luis@saffie.ca>
2024-01-11 10:00:34 -0500

    Footer spans accross

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="bg-ebp-header p-5 mx-5 mb-5">
-        <div className="text-gray-500 text-center">
+      <footer className="hidden sm:block w-full bg-ebp-header shadow-lg">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8"> 
+          <div className="flex justify-between items-center sm:py-5 text-white">
             <p>Contact us: info@earnbyplay.com</p>
             <p>© {new Date().getFullYear()} Earn by Play. All rights reserved.</p>
+          </div>
         </div>
-    </div>
+      </footer>
   );
 };
 
