e7655f6719fd6a87b0c6e14aab583ef276943ef9
lsaffie <luis@saffie.ca>
2024-01-25 12:17:49 -0500

    check for subscription status in /user-subscription

--- src/components/UserSubscription.jsx
+++ src/components/UserSubscription.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import appConfig from '../config';
 import ModalConfirm from './ModalConfirm'
+import { useUser } from '../UserContext'; // Import useUser hook
 
 const UserSubscription= ({ userId }) => {
   const [user, setUser] = useState(null);
@@ -9,6 +10,7 @@ const UserSubscription= ({ userId }) => {
   const [showModal, setShowModal] = useState(false);
   const [modalContent, setModalContent] = useState('');
   const [selectedSubscriptionId, setSelectedSubscriptionId] = useState(null);
+  const { currentUser } = useUser(); // Use useUser hook to get currentUser
 
 
   const getJwtToken = () => {
@@ -52,6 +54,31 @@ const UserSubscription= ({ userId }) => {
     });
   };
 
+  if (!currentUser?.active_subscription) {
+    return (
+      <>
+        <div className="px-0 my-5 sm:px-4 md:px-6 lg:px-8">
+          {" "}
+          {/* Responsive Padding */}
+          <div className="bg-white shadow overflow-hidden sm:rounded-sm p-3">
+            <div className="px-4 py-5 sm:px-6">
+              <h3 className="text-lg leading-6 font-medium text-gray-900">
+                Subscription Details
+              </h3>
+            </div>
+            <p className="text-md mt-3 text-red-500">
+              You're currently unsubscribed.
+            </p>
+            <a href="/subscribe"
+              className="inline-block text-center bg-green-500 text-white py-2 px-4 mt-4 rounded hover:bg-green-600 transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-green-300 shadow-lg text-sm font-medium" >
+              Subscribe
+            </a>
+          </div>
+        </div>
+      </>
+    );
+  }
+
   return (
     <div className="px-0 my-5 sm:px-4 md:px-6 lg:px-8"> {/* Responsive Padding */}
       {subscriptions.length > 0 ? (
@@ -59,7 +86,7 @@ const UserSubscription= ({ userId }) => {
           <div className="px-4 py-5 sm:px-6">
             <h3 className="text-lg leading-6 font-medium text-gray-900">Subscription Details</h3>
           </div>
-          {subscriptions.filter(subscription => subscription.status === 'Active').map((subscription, index) => (
+          {subscriptions.map((subscription, index) => (
             <div key={index}>
               <table className="min-w-full divide-y divide-gray-200 table-fixed">
                 <tbody className="bg-white">
@@ -90,6 +117,7 @@ const UserSubscription= ({ userId }) => {
                   </tr>
                 </tbody>
               </table>
+              <br />
               {subscription.status === 'Active' && (
                 <div className="px-4 py-4 sm:px-6 text-right">
                   <button
