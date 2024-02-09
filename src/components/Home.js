181aa37adff6d9cd43e1d358dd44bfbd937ada55
lsaffie <luis@saffie.ca>
2024-02-09 16:11:46 -0500

    homepage change

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import appConfig from '../config';
 import SignupForm from './SignupForm'
-import PhoneNumberVerificationHome from './PhoneNumberVerificationHome'
+import PhoneNumberVerification from './PhoneNumberVerification'
 
 
 const Home= () => {
@@ -13,13 +13,36 @@ const Home= () => {
   return (
     <div className="text-center min-h-screen h-auto mt-0">
 
-      <h2 className="text-2.5xl font-bold mb-4 text-center">
-        {/* <span className="text-ebp-cta-green-earn-font">Earn by Play </span>  */}
-        <div className="text-white">Turn Gameplay into Paydays!</div>
-      </h2>
+
+
+      <div className="p-5 mb-4 bg-ebp-header">
+        <h2 className="text-2.5xl font-bold mb-4 text-center">
+          <div className="text-white">Turn Gameplay into Paydays!</div>
+        </h2>
+
+        <h2 className="text-xl font-bold text-center text-white">
+          1. Choose a game
+        </h2>
+
+        <div className="text-gray-500 mb-2">
+          Pick a game from our selection
+        </div>
+
+        <h2 className="text-xl font-bold text-center text-white">
+          2. Complete a level
+        </h2>
+        <div className="text-gray-500 mb-2">Finish simple in-game tasks.</div>
+
+        <h2 className="text-xl font-bold text-center text-white">
+          3. Earn cash
+        </h2>
+        <div className="text-gray-500">
+          Earn coins for tasks, redeem for rewards.
+        </div>
+      </div>
 
       <div className="bg-ebp-header p-5">
-        <PhoneNumberVerificationHome />
+        <PhoneNumberVerification />
       </div>
 
     </div>
