e2bdd86dd1e827e7e4ead747ba6ee2bd76526b7c
lsaffie <luis@saffie.ca>
2024-02-14 20:09:07 -0500

    updating terms of service links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -50,7 +50,7 @@ const Footer = () => {
                 <a href="/privacy-policy" className="hover:text-gray-300">
                   Privacy Policy
                 </a>
-                <a href="/terms-of-service" className="hover:text-gray-300">
+                <a href="/terms" className="hover:text-gray-300">
                   Terms of Service
                 </a>
               </div>
